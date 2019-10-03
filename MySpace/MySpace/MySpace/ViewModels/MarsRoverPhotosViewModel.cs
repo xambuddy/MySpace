b4190391fb635a52831b19964c515c560c97be15
Dhave Dayao <xambuddy.ph@gmail.com>
2019-10-03 09:34:11 +0800

    Updated MarsRoverView

--- MySpace/MySpace/MySpace/ViewModels/MarsRoverPhotosViewModel.cs
+++ MySpace/MySpace/MySpace/ViewModels/MarsRoverPhotosViewModel.cs
@@ -4,28 +4,46 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace MySpace.ViewModels
 {
     public class MarsRoverPhotosViewModel : BaseViewModel
     {
         readonly DataService _dataService;
+
+        public int Page { get; set; }
         public MarsRoverPhotosViewModel()
         {
+            RefreshCommand = new Command(RefreshCommandExecute);
             _dataService = new DataService();
 
+            Page = 1;
+            RefreshCommandExecute();
+        }
+
+        private void RefreshCommandExecute()
+        {
             LoadMarsRoverPhotos();
+            Page++;
         }
 
         public ObservableCollection<MarsRoverPhoto> Photos { get; set; }
 
+        public Command RefreshCommand { get; set; }
+
+        private bool _isRefreshing;
+
+        public bool IsRefreshing { get => _isRefreshing; set => SetProperty(ref _isRefreshing, value); }
+
         private async void LoadMarsRoverPhotos()
         {
-            var photosRoot = await _dataService?.GetRoverPhotosAsync(1000, 1);
+            var photosRoot = await _dataService?.GetRoverPhotosAsync(1000, Page);
 
             if (photosRoot != null)
             {
                 Photos = new ObservableCollection<MarsRoverPhoto>(photosRoot.photos);
+                IsRefreshing = false;
             }
         }
     }
